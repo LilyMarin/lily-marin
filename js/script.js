47947e77fe245ec6c130b7ee04787cdb2ea419d8
LilyMarin <lili_marinova@yahoo.com>
2020-03-02 23:26:00 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,221 @@
+/* JQUERY CODES
+-------------------------------------------------*/
+
+$(document).ready(function() {
+
+	$.fn.animateRotate = function(angle, duration, easing, complete) {
+	  var args = $.speed(duration, easing, complete);
+	  var step = args.step;
+	  return this.each(function(i, e) {
+		args.complete = $.proxy(args.complete, e);
+		args.step = function(now) {
+		  $.style(e, 'transform', 'rotate(' + now + 'deg)');
+		  if (step) return step.apply(e, arguments);
+		};
+
+		$({deg: 0}).animate({deg: angle}, args);
+	  });
+	};
+	
+	$("#main-page").css("background-color", "#a26e7f");
+	$("#main-page").css("height", "100vh");
+	$("#main-page").css("width", "100%");
+	$("#main-page").fadeIn();
+	$(".maincontent").fadeIn();
+	
+	$(".mainlink").on("click", function() {
+		$(".maincontent").fadeOut();
+		$("#main-page").animate({
+			width: "25px",
+			height: "375px"
+		}, function() {
+			$(this).animateRotate(90);
+		});
+		
+		setTimeout(function() {
+			$("#main-page").fadeOut();		 
+		}, 1500);
+		
+		setTimeout(function() {
+			$("#next-page").animateRotate(0, 0);
+			$("#next-page").css("height", "25px");
+			$("#next-page").css("width", "375px");
+			$("#next-page").fadeIn();
+			$("#next-page").animate({
+				backgroundColor: "#ced2cd"
+			}, function() {
+				$(this).animate({
+					height: "100vh"
+				}, function() {
+					$(this).animate({
+						width: "100%"
+					}, function() {
+						$(".nextcontent").fadeIn(300);
+					});
+				});
+			});
+		}, 800);
+	});
+		
+	$(".nextlink").on("click", function() {
+		$(".nextcontent").fadeOut();
+		$("#next-page").animate({
+			width: "25px",
+			height: "375px"
+		}, function() {
+			$(this).animateRotate(-90);
+		});
+		
+		setTimeout(function() {
+			$("#next-page").fadeOut();			
+		}, 1500);
+		
+		setTimeout(function() {
+		$("#main-page").animateRotate(0, 0);
+		$("#main-page").css("height", "25px");
+		$("#main-page").css("width", "375px");
+			$("#main-page").fadeIn();
+			$("#main-page").animate({
+				height: "100vh"
+			}, function() {
+				$(this).animate({
+					width: "100%"
+				}, function() {
+					$(".maincontent").fadeIn(300);
+				});
+			});
+		}, 1400);
+	});
+
+
+
+	$(".mainlink-two").on("click", function() {
+		$(".maincontent").fadeOut();
+		$("#main-page").animate({
+			width: "25px",
+			height: "375px"
+		}, function() {
+			$(this).animateRotate(90);
+		});
+		
+		setTimeout(function() {
+			$("#main-page").fadeOut();		 
+		}, 1500);
+		
+		setTimeout(function() {
+			$("#next-page-two").animateRotate(0, 0);
+			$("#next-page-two").css("height", "25px");
+			$("#next-page-two").css("width", "375px");
+			$("#next-page-two").fadeIn();
+			$("#next-page-two").animate({
+				backgroundColor: "#ced2cd"
+			}, function() {
+				$(this).animate({
+					height: "100vh"
+				}, function() {
+					$(this).animate({
+						width: "100%"
+					}, function() {
+						$(".nextcontent-two").fadeIn(300);
+					});
+				});
+			});
+		}, 800);
+	});
+		
+	$(".nextlink-two").on("click", function() {
+		$(".nextcontent-two").fadeOut();
+		$("#next-page-two").animate({
+			width: "25px",
+			height: "375px"
+		}, function() {
+			$(this).animateRotate(-90);
+		});
+		
+		setTimeout(function() {
+			$("#next-page-two").fadeOut();			
+		}, 1500);
+		
+		setTimeout(function() {
+		$("#main-page").animateRotate(0, 0);
+		$("#main-page").css("height", "25px");
+		$("#main-page").css("width", "375px");
+			$("#main-page").fadeIn();
+			$("#main-page").animate({
+				height: "100vh"
+			}, function() {
+				$(this).animate({
+					width: "100%"
+				}, function() {
+					$(".maincontent").fadeIn(300);
+				});
+			});
+		}, 1400);
+	});
+	
+
+	$(".mainlink-three").on("click", function() {
+		$(".maincontent").fadeOut();
+		$("#main-page").animate({
+			width: "25px",
+			height: "375px"
+		}, function() {
+			$(this).animateRotate(90);
+		});
+		
+		setTimeout(function() {
+			$("#main-page").fadeOut();		 
+		}, 1500);
+		
+		setTimeout(function() {
+			$("#next-page-three").animateRotate(0, 0);
+			$("#next-page-three").css("height", "25px");
+			$("#next-page-three").css("width", "375px");
+			$("#next-page-three").fadeIn();
+			$("#next-page-three").animate({
+				backgroundColor: "#ced2cd"
+			}, function() {
+				$(this).animate({
+					height: "100vh"
+				}, function() {
+					$(this).animate({
+						width: "100%"
+					}, function() {
+						$(".nextcontent-three").fadeIn(300);
+					});
+				});
+			});
+		}, 800);
+	});
+		
+	$(".nextlink-three").on("click", function() {
+		$(".nextcontent-three").fadeOut();
+		$("#next-page-three").animate({
+			width: "25px",
+			height: "375px"
+		}, function() {
+			$(this).animateRotate(-90);
+		});
+		
+		setTimeout(function() {
+			$("#next-page-three").fadeOut();			
+		}, 1500);
+		
+		setTimeout(function() {
+		$("#main-page").animateRotate(0, 0);
+		$("#main-page").css("height", "25px");
+		$("#main-page").css("width", "375px");
+			$("#main-page").fadeIn();
+			$("#main-page").animate({
+				height: "100vh"
+			}, function() {
+				$(this).animate({
+					width: "100%"
+				}, function() {
+					$(".maincontent").fadeIn(300);
+				});
+			});
+		}, 1400);
+	});
+	
+});
